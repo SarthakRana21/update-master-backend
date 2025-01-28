3122742cc4c48d8bc88ad4e2448a9866164b90f0
SarthakRana21 <107024368+SarthakRana21@users.noreply.github.com>
2025-01-28 18:41:23 +0530

    backend

---
+++ src/getResponse.ts
@@ -0,0 +1,30 @@
+import OpenAI from "openai";
+import 'dotenv/config'
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY
+})
+
+const prompt ="You are Update Master ai agent Analyze the provided context and draft a clear, concise, and professional corporate update or repy. Ensure the tone is formal, the content is straight to the point, and avoid unnecessary details or flair. Focus on delivering key information effectively.";
+
+
+const completion = async (context: string) => {
+    
+    try {
+        const data = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            store: false,
+            messages: [
+                { role: 'assistant', content: prompt},
+                { role: 'user', content: `context: ${context}` }
+            ],
+            temperature: 0.1
+        })
+        const response = data.choices[0].message.content
+        return response
+
+    } catch (error) {
+        console.log('Error Completion: ', error)
+    }        
+}
+export default completion;
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import completion from './getResponse.js'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors({origin: "*", allowedHeaders: "content-type"}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).send('Update Master AI : )')
+})
+
+app.post('/api/v1/master', async (req, res) => {
+    const {context} = req.body;
+    const response = await completion(context)
+
+    res.status(201).send({
+        status: 'Success',
+        contextReceived: response
+    });
+})
+
+
+app.listen(port, () => {
+    console.log(`server is running on \nhttps://localhost:${port}`)
+})
